564b63ea14c06cac1ab4c9d60ea8e5fc3de5774b
Toasty3920 <119107482+Toasty3920@users.noreply.github.com>
2022-12-18 15:25:53 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+-- If you need help with setting up the script you can visit the TS-Scripts Discord and our team will help you: https://discord.gg/NXGjYDhH
+
+Config.RefreshTime = 5000                  -- Set the refresh time of the discord rich presence. Recommended is 5000 for performance optimization
+Config.AppID       = '1234567890'          -- Set the App ID from your Discord application
+Config.BigImage    = 'big_image'           -- Set asset for your application and set the name
+Config.SmallImage  = 'small_image'         -- Set another asset for your application and set the name
+Config.MaxPlayers  = 48                    -- Set maximum players which can play on your server
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+author 'ƬӨΛƧƬY'
+description 'Simple Discord Rich Presence Script from ƬӨΛƧƬY#7831. Visit the TS-Scripts Discord if you need help with setting up the script: https://discord.gg/NXGjYDhH'
+version '1.0'
+
+client_scripts {
+    'client/client.lua',
+    'config.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+}
